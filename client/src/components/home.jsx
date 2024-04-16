5695f96bff56ed52767eafe4dcfe860bbcee096e
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2024-04-15 21:08:55 -0700

    Google Gemini Post working

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -2,29 +2,29 @@ import { Fragment, useState } from 'react'
 import { FaceSmileIcon as FaceSmileIconOutline, PaperClipIcon } from '@heroicons/react/24/outline'
 import axios from 'axios';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI(process.env.REACT_APP_API_KEY);
-
+const genAI = new GoogleGenerativeAI(API_KEY);
 
 export default function Home() {
   const [userMessage, setUserMessage] = useState('');
 
   const sendMessage = async () => {
-    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-    const prompt = {userMessage};
-
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-
-    console.log(text);
+    try {
+      const model = await genAI.getGenerativeModel({ model: "gemini-pro" });
+      const result = await model.generateContent(userMessage);
+      const response = await result.response.text();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
     <div className="flex items-start space-x-4">
       <div className="min-w-0 flex-1">
-        <form action="#">
+        <form action="#" onSubmit={(e) => { e.preventDefault(); sendMessage(); }}>
           <div className="border-b border-gray-200 focus-within:border-teal-600">
             <label htmlFor="comment" className="sr-only">
               Add the URL or paste the content you would like to analyze...
@@ -54,7 +54,7 @@ export default function Home() {
             <div className="flex-shrink-0">
               <button
                 type="submit"
-                onClick={sendMessage}
+                onSubmit={(e) => { e.preventDefault(); sendMessage(); }}
                 className="inline-flex items-center rounded-md bg-teal-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
               >
                 Submit
