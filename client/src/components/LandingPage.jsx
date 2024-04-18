d49b3674f013d2def22008aecd52e9a65b2cfd12
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2024-04-18 10:21:57 -0700

    updated landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -20,34 +20,34 @@ const navigation = [
 ]
 const features = [
   {
-    name: 'Push to deploy',
+    name: 'Approval Log',
     description:
-      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+      'All of your submissions are aggregated, logged, and organized for seamless compliance reviews and audits.',
     icon: CloudArrowUpIcon,
   },
   {
-    name: 'SSL certificates',
+    name: 'End-to-end Encryption',
     description:
-      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+      'Your data is encrypted at rest and in transit, ensuring that your data is secure and compliant with industry standards.',
     icon: LockClosedIcon,
   },
   {
-    name: 'Simple queues',
+    name: 'Citing and Suggestions',
     description:
-      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+      'Marketing language is not just reviewed for compliance, but also includes suggestions for improvements and relevant SEC laws.',
     icon: ArrowPathIcon,
   },
   {
-    name: 'Advanced security',
+    name: 'Multiple File Formats',
     description:
-      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+      'Effective marketing requires multiple channels. Instead of restricting your marketing team, we accommodate them.',
     icon: FingerPrintIcon,
   },
 ]
 const tiers = [
   {
-    name: 'Freelancer',
-    id: 'tier-freelancer',
+    name: 'Monthly',
+    id: 'tier-monthly',
     href: '#',
     priceMonthly: '$24',
     description: 'The essentials to provide your best work for clients.',
@@ -55,10 +55,10 @@ const tiers = [
     mostPopular: false,
   },
   {
-    name: 'Startup',
-    id: 'tier-startup',
+    name: 'Annual',
+    id: 'tier-annual',
     href: '#',
-    priceMonthly: '$32',
+    priceMonthly: '$18',
     description: 'A plan that scales with your rapidly growing business.',
     features: [
       '25 products',
@@ -68,21 +68,6 @@ const tiers = [
       'Marketing automations',
     ],
     mostPopular: true,
-  },
-  {
-    name: 'Enterprise',
-    id: 'tier-enterprise',
-    href: '#',
-    priceMonthly: '$48',
-    description: 'Dedicated support and infrastructure for your company.',
-    features: [
-      'Unlimited products',
-      'Unlimited subscribers',
-      'Advanced analytics',
-      '1-hour, dedicated support response time',
-      'Marketing automations',
-    ],
-    mostPopular: false,
   },
 ]
 const footerNavigation = {
@@ -323,14 +308,13 @@ export default function LandingPage() {
             <div className="mx-auto max-w-4xl text-center">
               <h2 className="text-base font-semibold leading-7 text-cyan-600">Pricing</h2>
               <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-                Pricing plans for teams of&nbsp;all&nbsp;sizes
+                It pays for itself in the first month.
               </p>
             </div>
-            <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
-              Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas
-              in. Explicabo id ut laborum.
+            <p className="mx-auto mt-6 max-w-2xl text-center text-xl leading-8 text-gray-600">
+              The average compliance professional costs ~<strong>$65 an hour.</strong><br />That time should be spent on high-impact work.
             </p>
-            <div className="isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <div className="isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
               {tiers.map((tier, tierIdx) => (
                 <div
                   key={tier.id}
@@ -406,18 +390,14 @@ export default function LandingPage() {
           </div>
           <div className="mx-auto max-w-2xl text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Stop wasting your time on content reviews.
+              Stop wasting time reviewing <br />marketing collateral.
             </h2>
-            <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">
-              Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur
-              commodo do ea.
-            </p>
             <div className="mt-10 flex items-center justify-center">
               <a
                 href="#"
                 className="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
               >
-                Sign Up
+                Get Started
               </a>
             </div>
           </div>
