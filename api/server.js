69e2ff54a8b02ed060522db15ff5b5cafea6f16c
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2024-04-17 22:30:26 -0700

    updated user and auth routers

--- api/server.js
+++ api/server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 const path = require('path');
+const helmet = require('helmet');
 
 const usersRouter = require('./users/users-router');
 const authRouter = require('./auth/auth-router');
-const restricted = require('./middleware/restricted');
+const { validateEmptyFields, validateUsername, hashPassword } = require('./auth/auth-middleware');
 
-server.use(express.json());
+server.use(helmet());
 server.use(cors());
+server.use(express.json());
 
 server.use('/api/users', usersRouter);
-server.use('/api/auth', authRouter);
+server.use('/api/auth', authRouter, validateEmptyFields, validateUsername, hashPassword); 
 
 server.use((err, req, res, next) => {
   res.status(500).json({
