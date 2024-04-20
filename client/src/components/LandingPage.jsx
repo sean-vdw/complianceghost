4a53b5d452a3ee0eda1e538faf3bf8e9a23f8bc0
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2024-04-19 21:59:32 -0700

    updated CTA and pricing

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -307,85 +307,8 @@ export default function LandingPage() {
           </div>
         </div>
 
-        {/* Pricing section */}
-        <stripe-pricing-table
-          pricing-table-id="prctbl_1P7QHcGsmD2eSVUWdYjccxoS"
-          publishable-key="pk_live_51NC8lQGsmD2eSVUW4ynbOYWuyAd9hEkAmJGwiWsGn8CCtvVQwiJuct8XJiSaoPSDIiJL2H4A6aLezmE6LTdPb33O00z71twcGy"
-        >
-        </stripe-pricing-table>
-        {/* <div className="py-24 sm:pt-48">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto max-w-4xl text-center">
-              <h2 className="text-base font-semibold leading-7 text-cyan-600">Pricing</h2>
-              <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-                It pays for itself in the first month.
-              </p>
-            </div>
-            <p className="mx-auto mt-6 max-w-2xl text-center text-xl leading-8 text-gray-600">
-              The average compliance professional costs ~<strong>$65 an hour.</strong><br />That time should be spent on high-impact work.
-            </p>
-            <div className="isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-              {tiers.map((tier, tierIdx) => (
-                <div
-                  key={tier.id}
-                  className={classNames(
-                    tier.mostPopular ? 'lg:z-10 lg:rounded-b-none' : 'lg:mt-8',
-                    tierIdx === 0 ? 'lg:rounded-r-none' : '',
-                    tierIdx === tiers.length - 1 ? 'lg:rounded-l-none' : '',
-                    'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10'
-                  )}
-                >
-                  <div>
-                    <div className="flex items-center justify-between gap-x-4">
-                      <h3
-                        id={tier.id}
-                        className={classNames(
-                          tier.mostPopular ? 'text-cyan-600' : 'text-gray-900',
-                          'text-lg font-semibold leading-8'
-                        )}
-                      >
-                        {tier.name}
-                      </h3>
-                      {tier.mostPopular ? (
-                        <p className="rounded-full bg-cyan-600/10 px-2.5 py-1 text-xs font-semibold leading-5 text-cyan-600">
-                          Most popular
-                        </p>
-                      ) : null}
-                    </div>
-                    <p className="mt-4 text-sm leading-6 text-gray-600">{tier.description}</p>
-                    <p className="mt-6 flex items-baseline gap-x-1">
-                      <span className="text-4xl font-bold tracking-tight text-gray-900">{tier.priceMonthly}</span>
-                      <span className="text-sm font-semibold leading-6 text-gray-600">/month</span>
-                    </p>
-                    <ul role="list" className="mt-8 space-y-3 text-sm leading-6 text-gray-600">
-                      {tier.features.map((feature) => (
-                        <li key={feature} className="flex gap-x-3">
-                          <CheckIcon className="h-6 w-5 flex-none text-cyan-600" aria-hidden="true" />
-                          {feature}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <a
-                    href={tier.href}
-                    aria-describedby={tier.id}
-                    className={classNames(
-                      tier.mostPopular
-                        ? 'bg-cyan-600 text-white shadow-sm hover:bg-cyan-500'
-                        : 'text-cyan-600 ring-1 ring-inset ring-cyan-200 hover:ring-cyan-300',
-                      'mt-8 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600'
-                    )}
-                  >
-                    Buy plan
-                  </a>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div> */}
-
-        {/* CTA section */}
-        <div className="relative -z-10 mt-32 px-6 lg:px-8">
+        {/* Pricing section / CTA */}
+        <div className="relative columns-2 -z-10 mt-32 px-6 lg:px-8">
           <div
             className="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 transform-gpu justify-center overflow-hidden blur-3xl sm:bottom-0 sm:right-[calc(50%-6rem)] sm:top-auto sm:translate-y-0 sm:transform-gpu sm:justify-end"
             aria-hidden="true"
@@ -398,17 +321,16 @@ export default function LandingPage() {
               }}
             />
           </div>
-          <div className="mx-auto max-w-2xl text-center">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <div className="mx-auto max-w-2xl md:text-left text-center">
+            <h2 className="text-5xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               Stop wasting time reviewing <br />marketing collateral.
             </h2>
             <div className="mt-10 flex items-center justify-center">
-              <a
-                href="#"
-                className="rounded-md bg-cyan-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+              <stripe-pricing-table
+                pricing-table-id="prctbl_1P7QHcGsmD2eSVUWdYjccxoS"
+                publishable-key="pk_live_51NC8lQGsmD2eSVUW4ynbOYWuyAd9hEkAmJGwiWsGn8CCtvVQwiJuct8XJiSaoPSDIiJL2H4A6aLezmE6LTdPb33O00z71twcGy"
               >
-                Get Started
-              </a>
+              </stripe-pricing-table>
             </div>
           </div>
           <div
